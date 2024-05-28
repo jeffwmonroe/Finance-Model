f7fcd690ce28ba8e46559151801736c1c4b97929
Jeff Monroe <jeff@jeff-monroe.com>
2024-05-27 17:09:14 -0700

    second draft ACH

--- banking/__init__.py
+++ banking/__init__.py
@@ -2,3 +2,5 @@ from banking.check import Check
 from banking.process_checks import read_peachtree, process_checks, write_issue_void, read_pnc, process_pnc_initial, process_pnc_update
 
 from banking.ach import make_ach_payment
+from banking.natcha import Natcha
+from banking.payment_detail import PaymentDetail
