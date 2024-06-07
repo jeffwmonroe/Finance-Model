aeef4d2aa69327d02809eea73c7ac79b44563e06
Jeff Monroe <jeff@jeff-monroe.com>
2024-06-07 15:33:19 -0700

    more adjustments to ACH

--- banking/__init__.py
+++ banking/__init__.py
@@ -1,5 +1,12 @@
 from banking.check import Check
-from banking.process_checks import read_peachtree, process_checks, write_issue_void, read_pnc, process_pnc_initial, process_pnc_update
+from banking.process_checks import (read_peachtree,
+                                    process_checks,
+                                    write_issue_void,
+                                    read_pnc,
+                                    process_pnc_initial,
+                                    process_pnc_update,
+                                    calculate_outstanding_checks,
+                                    )
 
 from banking.ach import make_ach_payment
 from banking.natcha import Natcha
